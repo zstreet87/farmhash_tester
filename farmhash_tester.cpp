a7f910e17cfd01c9500e9e8e57d6332641106049
Zachary Streeter <Zachary.Streeter@amd.com>
2023-11-10 21:43:00 +0000

    added lil more tf

--- farmhash_tester.cpp
+++ farmhash_tester.cpp
@@ -29,7 +29,7 @@ __device__ __forceinline__ void FillDigits(T val, int num_digits, int *i,
 template <typename T>
 __device__ __forceinline__ int IntegerToString(T val, char *buf) {
   int num_digits = 0;
-  T val_a = val;
+  int8_t val_a = val;
   do {
     val_a = val_a / 10;
     num_digits++;
@@ -66,7 +66,7 @@ int main() {
 
   const int length = 1;
 
-  using templateType = int;
+  using templateType = int8_t;
 
   templateType inputCPU = 1;
   templateType *inputGPU;
@@ -91,9 +91,8 @@ int main() {
   int num_blocks = 24;
   int threads_per_block = 1024;
 
-  hipLaunchKernelGGL(kernel<templateType>, dim3(num_blocks),
-                     dim3(threads_per_block), smem_bytes_per_block, 0, inputGPU,
-                     gpuHashResult);
+  GpuLaunchKernel(kernel<templateType>, num_blocks, threads_per_block, 
+		  smem_bytes_per_block, 94274085643200, inputGPU, gpuHashResult);
 
   if (hipDeviceSynchronize() != hipSuccess) {
     std::cerr << "hipDeviceSynchronize failed" << std::endl;
--- gpu_kernel_helper.h
+++ gpu_kernel_helper.h
@@ -7,4 +7,19 @@
 #define GPU_1D_KERNEL_LOOP(i, n) \
   for (int i : ::tensorflow::GpuGridRangeX<int>(n))
 
+// Launches a GPU kernel through cudaLaunchKernel in CUDA environment, or
+// hipLaunchKernel in ROCm environment with the given arguments.
+//
+// The kernel parameters 'Ts' must be constructible from the arguments 'Args'.
+template <typename... Ts, typename... Args>
+void GpuLaunchKernel(void (*function)(Ts...), dim3 grid_dim, dim3 block_dim,
+                       size_t shared_memory_size_bytes, hipStream_t stream,
+                       Args... arguments) {
+  if (grid_dim.x * grid_dim.y * grid_dim.z > 0 &&
+      block_dim.x * block_dim.y * block_dim.z > 0) {
+    hipLaunchKernelGGL(function, grid_dim, block_dim, shared_memory_size_bytes,
+                       stream, std::forward<Args>(arguments)...);
+  }
+}
+
 #endif  // GPU_KERNEL_HELPER_H_
